b930ba82f33dc771f479a98cd541954d9a53312d
mattykurlzz <mamontovvanya@yandex.ru>
2024-09-22 14:47:33 +0300

    add report lab1

--- lab1/src/func.cpp
+++ lab1/src/func.cpp
@@ -2,15 +2,12 @@
 #include <typeinfo>
 using namespace std;
 int f() {
-	short var1 = 1;
-	double var2 {3};
+	int var1 = 1;
+	float var2 {3};
 	cout << typeid(var1).name() << '(' << var1 << ") " << "+" << ' ' << typeid(var2).name() << '(' << var2 << ") = " << typeid(var1 +var2).name() << '(' << (var1 + var2) << ')' << endl;
 	cout << typeid(var1).name() << '(' << var1 << ") " << "-" << ' ' << typeid(var2).name() << '(' << var2 << ") = " << typeid(var1 -var2).name() << '(' << (var1 - var2) << ')' << endl;
 	cout << typeid(var1).name() << '(' << var1 << ") " << "/" << ' ' << typeid(var2).name() << '(' << var2 << ") = " << typeid(var1 /var2).name() << '(' << (var1 / var2) << ')' << endl;
-	cout << typeid(var1).name() << '(' << var1 << ") " << "%" << ' ' << typeid(var2).name() << '(' << var2 << ") = " << typeid(var1 %var2).name() << '(' << (var1 % var2) << ')' << endl;
 	cout << typeid(var1).name() << '(' << var1 << ") " << "*" << ' ' << typeid(var2).name() << '(' << var2 << ") = " << typeid(var1 *var2).name() << '(' << (var1 * var2) << ')' << endl;
 	cout << typeid(var1).name() << '(' << var1 << ") " << "|" << ' ' << typeid(var2).name() << '(' << var2 << ") = " << typeid(var1 |var2).name() << '(' << (var1 | var2) << ')' << endl;
-	cout << typeid(var1).name() << '(' << var1 << ") " << "&" << ' ' << typeid(var2).name() << '(' << var2 << ") = " << typeid(var1 &var2).name() << '(' << (var1 & var2) << ')' << endl;
-	cout << typeid(var1).name() << '(' << var1 << ") " << "^" << ' ' << typeid(var2).name() << '(' << var2 << ") = " << typeid(var1 ^var2).name() << '(' << (var1 ^ var2) << ')' << endl;
 	return 0;
 }
